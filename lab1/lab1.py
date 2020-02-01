5adceb262ff156fa023bc2a7c7c09372e758f9f1
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-02-01 00:10:42 -0500

    4/5 functions done

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,25 +2,80 @@ from lab1_utilities import *
 
 
 def get_installations_from_file(file_name):
-    # implement
-    pass
+    '''
+    Parses data into Installation objects and returns as list
+    '''
+
+    output = []
+
+    with open(file_name, "r") as f:
+        lines = f.readlines() # gets lines prepped and removes the top one
+        lines.pop(0)
+
+        for l in lines:
+            fields = l.strip("\n").split("\t")
+
+            output.append(Installation(
+                name=fields[0], 
+                ward = int(fields[2]), 
+                position = (float(fields[7]), float(fields[8])), 
+                indoor=(fields[15]=="INDOORS")))
+            # process the things - str, int, (float, float), bool
+
+    return output # type list of installations
 
 
 def euclidean_distance(position1, position2):
-    # implement
-    pass
+    '''
+    returns sqrt((y2-y1)^2 + (x2-x1)^2)
+    uses lazy version of sqrt without needing to import anything
+    '''
+    return ((position2[1]-position1[1])**2 + (position2[0]-position1[0])**2)**(1/2) # DONE
 
 
 def get_adjacency_mtx(installations):
-    # implement
-    pass
+    '''
+    Create adjacency matrix from installations data
+    '''
+    M = [[0] * len(installations) for i in range(len(installations))] # prefill matrix
 
+    for x in range(0, len(installations)):
+
+        for y in range(0, len(installations)):
+            if abs(installations[y].ward-installations[x].ward) <= 1 and y != x:
+                M[x][y] = euclidean_distance(installations[x].position, installations[y].position) * (1.5 if (installations[x].indoor or installations[y].indoor) else 1.0)
+            
+    return M
 
 def make_graph(installations):
-    # implement
-    pass
+    '''
+    Combine adjacency matrix and collections list into graph object
+    '''
+    # uses previous functions and string names
+    return Graph([j.name for j in installations], get_adjacency_mtx(installations))
 
 
 def find_shortest_path(installation_A, installation_B, graph):
-    # implement
+    '''
+    Djikstra's Algorithm implemented on this
+    
+    '''
     pass
+
+if __name__ == "__main__":
+    print(euclidean_distance((-1, -1),(1, 1)))
+    l = get_installations_from_file("test_data.txt")
+
+    for j in l:
+        print(j)
+        '''
+        print(j.position)
+        print(j.indoor)
+        '''
+    
+    M = get_adjacency_mtx(l)
+    print(M)
+
+    print(make_graph(l))
+
+    
