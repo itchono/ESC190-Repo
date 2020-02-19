214404a5fe16166aff862e26f9d0f01e395a9c4c
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-02-19 01:24:34 -0500

    Update lab2.c

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,4 +1,5 @@
 #include "lab2.h"
+#include <math.h>
 
 void print_tree(float arr[], int n) {
     // breadth first traversal
@@ -11,7 +12,14 @@ void print_tree(float arr[], int n) {
     while (tempSize > 0) {
         numRows ++;
         tempSize /= 2;
+        // shitty logarithm function
     }
+
+    /*
+    printf("%d",numRows);
+    printf(" Rows.\nPrint Tree:\n");
+    debug
+    */
 
     queue[0] = 0; // enqueue root
 
@@ -21,59 +29,94 @@ void print_tree(float arr[], int n) {
 
     int rowNumber = 0;
 
+
     while (queueSize > 0) {
-        float* level = calloc(n, sizeof(float));
-        int levelptr = 0;
+        int* level = calloc(n, sizeof(int)); // a sub-list of the current level
+        int levelptr = 0; // indicate which level we are currently on
 
-        for (int x = queueStart; x < queueEnd; x++) {
+        /*
+        printf("Size: ");
+        printf("%d", queueSize);
+        printf(" Start: ");
+        printf("%d", queueStart);
+        printf(" End: ");
+        printf("%d", queueEnd);
+        printf("\n");
+        debug code
+        */
+        int qs = queueStart; // fixed pointers for sizing
+        int qe = queueEnd;
 
-            level[levelptr] = arr[queue[x]];
-            levelptr ++; // enqueue current node at a level
-            
-            int i = queue[x];
-            queueStart ++; // dequeue all the nodes here.
+        for (int x = qs; x < qe; x++) {
+          // NOTE TO SELF: c will re-check boundary values if they get updated.
+
+            int i = queue[x]; // index which we will be putting through arr to output.
+
+            /*printf("%f", arr[i]);
+            printf("\n");
+            debug code
+            */
+
+            queueStart ++; // dequeue the current node we are on
             queueSize --;
 
+            level[levelptr] = i;
+            levelptr ++; // enqueue current node at a level
+
             if (get_left_value(arr, n, i) != -1) {
-                printf("leftOK\n");
                 queue[queueEnd] = 2*i+1;
                 queueEnd ++; // enqueue
                 queueSize ++;
             }
             if (get_right_value(arr, n, i) != -1) {
-                printf("rightOK\n");
                 queue[queueEnd] = 2*i+2;
                 queueEnd ++; // enqueue
                 queueSize ++;
             }
         }
+        rowNumber ++;
 
-        for (int x = 0; x <= levelptr; x++) {
-            printf("%d", level[x]);
+        // PRINTING STUFF OUT
+        // Stage 1: Pre-spacing
+        for (int y = 0; y < 3*(numRows-rowNumber); y++)  {
+          printf(" ");
         }
+
+
+        // Stage 2: Values
+        for (int x = 0; x < levelptr; x++) {
+            printf("%g", arr[level[x]]);
+
+            for (int y = 0; y < 4*(numRows-rowNumber + 1); y++) {
+              printf(" ");
+            }
+        }
+
+        free(level);
         printf("\n");
 
         
     }
 
-    printf("%d",numRows);
-
 }
 
 float get_parent_value(float arr[], int n, int index) {
+    // DONE
     // parent is (n-1)/2
-    return (index > (n-1) || index == 0) ? -1 : arr[(index-1)/2];
+    return (index > (n-1) || index <= 0) ? -1 : arr[(index-1)/2];
 }
 
 float get_left_value(float arr[], int n, int index) {
+    // DONE
     // left is 2n+1
-    return (2*index + 1 > (n-1)) ? -1 : arr[index*2 + 1];
+    return (2*index + 1 > (n-1) || index < 0) ? -1 : arr[index*2 + 1];
 }
 
 float get_right_value(float arr[], int n, int index) {
+    // DONE
     // right is 2n+2
    
-    return (2*index + 2 > (n-1)) ? -1 : arr[index*2 + 2];
+    return (2*index + 2 > (n-1) || index < 0) ? -1 : arr[index*2 + 2];
 }
 
 int is_max_heap(float arr[], int n) {
