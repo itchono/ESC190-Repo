41b5b8e5eec4839d07118feb471c96687514d5f8
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-03-04 23:39:31 -0500

    Execercise?

---
+++ THATS IN C/C_ex/week8.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LLNode {
+    struct LLNode* prev;
+    struct LLNode* next;
+    double value;
+
+};
+
+struct LL {
+    struct LLNode* head;
+    struct LLNode* tail;
+};
+
+void delete_node(struct LL* list,int i);
+void insert_node(struct LL* list, int i, double value);
+void delete_list(struct LL* list);
