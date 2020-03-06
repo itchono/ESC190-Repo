8e53754c2993d037dfeb040d769a4be2be729af3
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-03-06 02:19:45 -0500

    Exercieses

--- THATS IN C/C_ex/Week 8 Exercises/week8.h
+++ THATS IN C/C_ex/Week 8 Exercises/week8.h
@@ -11,8 +11,10 @@ struct LLNode {
 struct LL {
     struct LLNode* head;
     struct LLNode* tail;
+    int size;
 };
 
 void delete_node(struct LL* list,int i);
 void insert_node(struct LL* list, int i, double value);
 void delete_list(struct LL* list);
+struct LL* init_list();
