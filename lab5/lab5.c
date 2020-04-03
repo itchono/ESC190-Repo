af55183f99868769a49407012f336bb3d60e7bbb
Mingde Yin <54449457+itchono@users.noreply.github.com>
2020-04-03 04:01:07 -0400

    Lab5 Part 1 Done

---
+++ lab5/lab5.c
@@ -0,0 +1,88 @@
+//include things here
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+/*
+
+Fill in the following functions - follow the document for proper declaration and definition
+1. encodeNuc
+2. decodeBin
+3. findProtein
+4. proteinReport
+5. isolateProtein
+6. genMutant
+
+Implement for 1% bonus!
+7. checkMutant
+
+*/
+
+void encodeNuc(char *filename) {
+
+    char outName[256] = {'b', '\0'};
+    
+    strcat(outName, filename);
+
+    FILE* fin = fopen(filename, "r");
+    FILE* fout = fopen(outName, "w");
+
+    char seq = 0;
+
+    while(fscanf(fin, "%c", &seq) != EOF) {
+        switch (seq) {
+            case 'A':
+            fprintf(fout, "00");
+            break;
+            case 'C':
+            fprintf(fout, "01");
+            break;
+            case 'G':
+            fprintf(fout, "10");
+            break;
+            case 'T':
+            fprintf(fout, "11");
+            break;
+        }
+    }
+
+
+    fclose(fout);
+    fclose(fin);
+}
+
+void decodeBin(char *filename) {
+
+    char outName[256] = {'n', '\0'};
+    strcat(outName, filename);
+
+    FILE* fin = fopen(filename, "r");
+    FILE* fout = fopen(outName, "w");
+
+    char seq1 = 0;
+    char seq2 = 0;
+    char read[3];
+
+    while(fscanf(fin, "%2c", &read) != EOF) {
+        seq1 = read[0]-48;
+        seq2 = read[1]-48;
+        if (!seq1 && !seq2) fprintf(fout, "A");
+        else if (!seq1 && seq2) fprintf(fout, "C");
+        else if (seq1 && !seq2) fprintf(fout, "G");
+        else if (seq1 && seq2) fprintf(fout, "T");
+    }
+    fclose(fout);
+    fclose(fin);
+}
+
+int main() {
+
+    encodeNuc("partialSLV.txt");
+    decodeBin("bpartialSLV.txt");
+
+    return 0;
+}
+
+
